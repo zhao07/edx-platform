9cfb4b6324f062628b48a7cc461f0fd3a93da139
Frances Botsford <frances@edx.org>
2013-11-08 09:55:36 -0500

    added js to render iframe as part of DOM in Studio base.js

--- cms/static/js/base.js
+++ cms/static/js/base.js
@@ -66,6 +66,20 @@ domReady(function() {
         }
     });
 
+
+    // render iframe in the DOM rather than on top of it
+    $("iframe").each(function(){
+        var ifr_source = $(this).attr('src');
+        var wmode = "wmode=transparent";
+        if(ifr_source.indexOf('?') != -1) {
+            var getQString = ifr_source.split('?');
+            var oldString = getQString[1];
+            var newString = getQString[0];
+            $(this).attr('src',newString+'?'+wmode+'&'+oldString);
+        }
+        else $(this).attr('src',ifr_source+'?'+wmode);
+    });
+
     // general link management - new window/tab
     $('a[rel="external"]').attr('title', gettext('This link will open in a new browser window/tab')).bind('click', linkNewWindow);
 
