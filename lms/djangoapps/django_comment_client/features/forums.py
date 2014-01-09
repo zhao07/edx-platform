558110106afc204e5fec293aea737d9544aa926b
jsa <jsa@edx.org>
2014-01-09 14:30:32 -0500

    work in progress, elaborating on mock_cs_server, but likely to abandon this.

--- lms/djangoapps/django_comment_client/features/forums.py
+++ lms/djangoapps/django_comment_client/features/forums.py
@@ -1,6 +1,27 @@
-from lettuce import world, step
+import datetime
+import json
+import threading
+import uuid
+
+from lettuce import world, step, before, after
 
 from django_comment_common.utils import seed_permissions_roles
+from django_comment_client.tests.mock_cs_server.mock_cs_server import MockCommentServiceServer
+
+CS = None
+
+@before.all
+def start_mock_cs_server():
+    global CS
+    s = CS = MockCommentServiceServer(port_num=4567, response={})
+    t = threading.Thread(target=s.serve_forever)
+    t.daemon = True
+    t.start()
+
+@after.all
+def stop_mock_cs_server(step):
+    global CS
+    CS.shutdown()
 
 @step(u'I visit the discussion tab')
 def visit_the_discussion_tab(step):
@@ -20,6 +41,33 @@ def create_thread(step):
     world.css_click('.new-post-btn')
     world.css_fill('#new-post-title', 'this is the new post title')
     world.css_fill('#wmd-input-new-post-body-undefined', 'this is the new post body')
+    CS._response_str = json.dumps({
+        "body": 'this is the new post body',
+        "anonymous_to_peers": False,
+        "votes": {
+            "count": 0,
+            "down_count": 0,
+            "point": 0,
+            "up_count": 0
+            },
+        "user_id": 1,
+        "anonymous": False,
+        "title": 'this is the new post title',
+        "username": "robot",
+        "created_at": datetime.datetime.now().isoformat(),
+        "tags": [],
+        "updated_at": datetime.datetime.now().isoformat(),
+        "commentable_id": world.browser.execute_script('$(".topic")[0].attributes["data-discussion_id"]'),
+        "abuse_flaggers": [],
+        "comments_count": 0,
+        "closed": False,
+        "course_id": 'edx/999/Test_Course',
+        "at_position_list": [],
+        "type": "thread",
+        "id": str(uuid.uuid4()).replace('-',''),
+        "pinned": False
+        })
+
     world.css_click('#new-post-submit')
     world.wait_for_js_to_load()
     assert world.css_value('div.discussion-post h1') == 'this is the new post title'
--- lms/djangoapps/django_comment_client/tests/mock_cs_server/mock_cs_server.py
+++ lms/djangoapps/django_comment_client/tests/mock_cs_server/mock_cs_server.py
@@ -1,5 +1,7 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import datetime
 import json
+import uuid
 from logging import getLogger
 logger = getLogger(__name__)
 
@@ -10,40 +12,57 @@ class MockCommentServiceRequestHandler(BaseHTTPRequestHandler):
     '''
     protocol = "HTTP/1.0"
 
+    def _get_json(self):
+        '''
+        '''
+        # Retrieve the POST or PUT data into a dict.
+        # It should have been sent in json format
+        length = int(self.headers.getheader('content-length'))
+        data_string = self.rfile.read(length)
+        return json.loads(data_string)
+
+    def _return_failure(self, status=500, reason='Internal Server Error'):
+        '''
+        '''
+        self.send_response(status, reason)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        return False
+
+    def _handle(self, method):
+        '''
+        '''
+        if method in ('POST', 'PUT'):
+            try:
+                post_dict = self._get_json()
+            except Exception, e:
+                pass
+        else:
+            post_dict = None
+
+        logger.debug("Comment Service received {} request to path {}".format(
+                    method, self.path))
+
+        if not 'X-Edx-Api-Key' in self.headers:
+            return self._return_failure(reason='Unauthorized')
+
+        response = self.server._response_str
+        # Log the response
+        logger.debug("Comment Service: sending response %s" % json.dumps(response))
+
+        # Send a response back to the client
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(response)
+
     def do_POST(self):
         '''
         Handle a POST request from the client
         Used by the APIs for comment threads, commentables, comments,
         subscriptions, commentables, users
         '''
-        # Retrieve the POST data into a dict.
-        # It should have been sent in json format
-        length = int(self.headers.getheader('content-length'))
-        data_string = self.rfile.read(length)
-        post_dict = json.loads(data_string)
-
-        # Log the request
-        logger.debug("Comment Service received POST request %s to path %s" %
-                    (json.dumps(post_dict), self.path))
-
-        # Every good post has at least an API key
-        if 'X-Edx-Api-Key' in self.headers:
-            response = self.server._response_str
-            # Log the response
-            logger.debug("Comment Service: sending response %s" % json.dumps(response))
-
-            # Send a response back to the client
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(response)
-
-        else:
-            # Respond with failure
-            self.send_response(500, 'Bad Request: does not contain API key')
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            return False
+        return self._handle('POST')
 
     def do_PUT(self):
         '''
@@ -51,34 +70,13 @@ class MockCommentServiceRequestHandler(BaseHTTPRequestHandler):
         Used by the APIs for comment threads, commentables, comments,
         subscriptions, commentables, users
         '''
-        # Retrieve the PUT data into a dict.
-        # It should have been sent in json format
-        length = int(self.headers.getheader('content-length'))
-        data_string = self.rfile.read(length)
-        post_dict = json.loads(data_string)
+        return self._handle('PUT')
 
-        # Log the request
-        logger.debug("Comment Service received PUT request %s to path %s" %
-                    (json.dumps(post_dict), self.path))
-
-        # Every good post has at least an API key
-        if 'X-Edx-Api-Key' in self.headers:
-            response = self.server._response_str
-            # Log the response
-            logger.debug("Comment Service: sending response %s" % json.dumps(response))
-
-            # Send a response back to the client
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(response)
-
-        else:
-            # Respond with failure
-            self.send_response(500, 'Bad Request: does not contain API key')
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            return False
+    def do_GET(self):
+        '''
+        Handle a GET request from the client
+        '''
+        return self._handle('GET')
 
 
 class MockCommentServiceServer(HTTPServer):
