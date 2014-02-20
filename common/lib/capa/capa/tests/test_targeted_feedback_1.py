7e33bf5310e22e95d9f830615731937be5991ad3
trbm <trbm@stanford.edu>
2014-02-20 13:19:37 -0800

    fixed the pro forma True test (to satisfy the coverage analysis)

--- common/lib/capa/capa/tests/test_targeted_feedback_1.py
+++ common/lib/capa/capa/tests/test_targeted_feedback_1.py
@@ -682,7 +682,7 @@ class CapaTargetedFeedbackTest(unittest.TestCase):
         testcases.append((False, r"targetedfeedback explanation-id=\"feedbackC\"", "feedback item `feedbackC` should not be visible"))
         testcases.append((False, r"Incorrect", "no targeted feedback should be shown "))
         testcases.append((False, r"Correct", "no targeted feedback should be shown "))
-        testcases.append((True, r"Problem", ""))       # this test is included here just to satisfy the coverage test
+        testcases.append((True, r"What is the correct answer?", ""))       # this test is included here just to satisfy the coverage test
 
         for testcase in testcases:
             expect_match, pattern, message = testcase
@@ -705,7 +705,7 @@ class CapaTargetedFeedbackTest(unittest.TestCase):
         testcases.append((False, r"targetedfeedback explanation-id=\"feedbackC\"", "feedback item `feedbackC` should not be visible"))
         testcases.append((False, r"Incorrect", "no targeted feedback should be shown"))
         testcases.append((False, r"Correct", "no targeted feedback should be shown "))
-        testcases.append((True, r"Problem", ""))       # this test is included here just to satisfy the coverage test
+        testcases.append((True, r"What is the correct answer?", ""))       # this test is included here just to satisfy the coverage test
 
         for testcase in testcases:
             expect_match, pattern, message = testcase
